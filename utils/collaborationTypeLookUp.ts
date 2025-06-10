d5e1f5b7b71043daa72b59f716db229053b14ca8
jkwonny <justinkwon123@gmail.com>
2025-06-10 01:27:57 -0400

    collaboration type

--- utils/collaborationTypeLookUp.ts
+++ utils/collaborationTypeLookUp.ts
@@ -1,5 +1,5 @@
 export const collaborationTypeLookUp = {
-    'no_minimum_spend': 'Open Space',
+    'no_minimum_spend': 'No Minimum Spend',
     'flat': 'Flat Fee',
     'open_venue': 'Open Space',
     'minimum_spend': 'Minimum Spend',
